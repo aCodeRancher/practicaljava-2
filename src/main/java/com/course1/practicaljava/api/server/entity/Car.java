072b2e66f2ba595117c9c2ac5de0d9df2ad8589b
helen <hhma@alumni.cpp.edu>
2022-07-01 22:31:28 -0400

    add Car entity.

---
+++ src/main/java/com/course1/practicaljava/api/server/entity/Car.java
@@ -0,0 +1,16 @@
+package com.course1.practicaljava.api.server.entity;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Car {
+
+    private String brand;
+    private String color;
+    private String type;
+}
