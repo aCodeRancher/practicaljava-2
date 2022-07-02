0f878330fc9e2629e7c2620b7e4689b5b4d92034
helen <hhma@alumni.cpp.edu>
2022-07-01 22:31:54 -0400

    add CarApi

--- src/main/java/com/course1/practicaljava/api/server/CarApi.java
+++ src/main/java/com/course1/practicaljava/api/server/CarApi.java
@@ -2,17 +2,18 @@ package com.course1.practicaljava.api.server;
 
 import com.course1.practicaljava.api.server.entity.Car;
 import com.course1.practicaljava.service.CarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequestMapping(value="/api/car/v1")
 @RestController
 public class CarApi {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CarApi.class);
     @Autowired
     private CarService carService;
 
@@ -21,6 +22,12 @@ public class CarApi {
          return carService.generateCar();
      }
 
+     @PostMapping(value="/echo", consumes= MediaType.APPLICATION_JSON_VALUE,
+                  produces = MediaType.APPLICATION_JSON_VALUE)
+     public Car echo(@RequestBody Car car){
+        LOG.info("Car is {}", car);
 
+        return car ;
+     }
 
 }
