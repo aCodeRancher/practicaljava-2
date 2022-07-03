e00aec8b4e77741ebdb90a6c36f1b6faed245fc6
helen <hhma@alumni.cpp.edu>
2022-07-02 21:35:12 -0400

    add additionalFeatures

--- src/main/java/com/course1/practicaljava/api/server/entity/Car.java
+++ src/main/java/com/course1/practicaljava/api/server/entity/Car.java
@@ -3,6 +3,9 @@ package com.course1.practicaljava.api.server.entity;
 
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,4 +16,11 @@ public class Car {
     private String brand;
     private String color;
     private String type;
+
+    private int price;
+    private boolean available;
+    private LocalDate firstRelease;
+
+    private List<String> additionalFeatures;
+
 }
