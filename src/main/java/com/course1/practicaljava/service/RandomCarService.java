328de18f59fcb75748203bfbcee3628669be1bac
helen <hhma@alumni.cpp.edu>
2022-07-02 21:36:37 -0400

    add more attributes to random car service

--- src/main/java/com/course1/practicaljava/service/RandomCarService.java
+++ src/main/java/com/course1/practicaljava/service/RandomCarService.java
@@ -1,8 +1,10 @@
 package com.course1.practicaljava.service;
 
 import com.course1.practicaljava.api.server.entity.Car;
+import com.course1.practicaljava.util.RandomDateUtil;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
@@ -13,7 +15,14 @@ public class RandomCarService implements CarService{
           var brand = BRANDS.get(ThreadLocalRandom.current().nextInt(0, BRANDS.size()));
           var color = COLORS.get(ThreadLocalRandom.current().nextInt(0,COLORS.size()));
           var type = TYPES.get(ThreadLocalRandom.current().nextInt(0,TYPES.size()));
-
-        return  new Car(brand , color, type);
+          var available = ThreadLocalRandom.current().nextBoolean();
+          var price = ThreadLocalRandom.current().nextInt(5000, 12001);
+          var firstReleaseDate = RandomDateUtil.generateRandomLocalDate();
+          int randomCount = ThreadLocalRandom.current().nextInt(ADDITIONAL_FEATURES.size());
+          var additionalFeatures = new ArrayList<String>();
+          for (int i=0;i<randomCount;i++){
+              additionalFeatures.add(ADDITIONAL_FEATURES.get(i));
+          }
+        return  new Car(brand , color, type, price, available, firstReleaseDate, additionalFeatures);
     }
 }
