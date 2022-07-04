046f111c9a0bac1430338034dd3f7255f26fae62
helen <hhma@alumni.cpp.edu>
2022-07-03 20:38:36 -0400

    add JsonNaming

--- src/main/java/com/course1/practicaljava/api/server/entity/Car.java
+++ src/main/java/com/course1/practicaljava/api/server/entity/Car.java
@@ -1,6 +1,11 @@
 package com.course1.practicaljava.api.server.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -11,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Car {
 
     private String brand;
@@ -19,8 +25,16 @@ public class Car {
 
     private int price;
     private boolean available;
+    @JsonFormat(pattern="dd-MM-yyyy", timezone = "America/New_York")
     private LocalDate firstRelease;
-
+    @JsonInclude(value= JsonInclude.Include.NON_EMPTY)
+    private String secretFeature;
+    @JsonInclude(value= JsonInclude.Include.NON_EMPTY)
     private List<String> additionalFeatures;
+
+    @JsonUnwrapped
+    private Engine engine;
+    private List<Tire> tires;
+
 
 }
