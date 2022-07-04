628b0daea1d0ed93b45b673ff7b58c38314fea07
helen <hhma@alumni.cpp.edu>
2022-07-03 20:40:31 -0400

    add secret feature

--- src/main/java/com/course1/practicaljava/service/RandomCarService.java
+++ src/main/java/com/course1/practicaljava/service/RandomCarService.java
@@ -1,6 +1,8 @@
 package com.course1.practicaljava.service;
 
 import com.course1.practicaljava.api.server.entity.Car;
+import com.course1.practicaljava.api.server.entity.Engine;
+import com.course1.practicaljava.api.server.entity.Tire;
 import com.course1.practicaljava.util.RandomDateUtil;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +21,30 @@ public class RandomCarService implements CarService{
           var price = ThreadLocalRandom.current().nextInt(5000, 12001);
           var firstReleaseDate = RandomDateUtil.generateRandomLocalDate();
           int randomCount = ThreadLocalRandom.current().nextInt(ADDITIONAL_FEATURES.size());
+          var fuel = FUELS.get(ThreadLocalRandom.current().nextInt(FUELS.size()));
+          var horsePower = ThreadLocalRandom.current().nextInt(100,200);
+          var engine = new Engine();
+          engine.setFuelType(fuel);
+          engine.setHorsePower(horsePower);
+
+           var tires = new ArrayList<Tire>();
+           for (int i=0; i<3; i++){
+               var tire = new Tire();
+               var manufacturer =
+                       TIRE_MANUFACTURERS.get(ThreadLocalRandom.current().nextInt(TIRE_MANUFACTURERS.size()));
+               var size = ThreadLocalRandom.current().nextInt(15,18);
+               var tirePrice = ThreadLocalRandom.current().nextInt(200,401);
+               tire.setManufacturer(manufacturer);
+               tire.setPrice(tirePrice);
+               tire.setSize(size);
+               tires.add(tire);
+           }
+
           var additionalFeatures = new ArrayList<String>();
           for (int i=0;i<randomCount;i++){
               additionalFeatures.add(ADDITIONAL_FEATURES.get(i));
           }
-        return  new Car(brand , color, type, price, available, firstReleaseDate, additionalFeatures);
+          var secretFeature = ThreadLocalRandom.current().nextBoolean()? "Can fly":null;
+        return  new Car(brand , color, type, price, available, firstReleaseDate,secretFeature, additionalFeatures,engine,tires);
     }
 }
