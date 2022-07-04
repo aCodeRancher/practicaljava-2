4f00a4752608ffc7c93c44aca6e4ff9f7605f817
helen <hhma@alumni.cpp.edu>
2022-07-03 22:25:56 -0400

    add @Document, and @Id

--- src/main/java/com/course1/practicaljava/api/server/entity/Car.java
+++ src/main/java/com/course1/practicaljava/api/server/entity/Car.java
@@ -7,6 +7,11 @@ import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,15 +22,20 @@ import java.util.List;
 @AllArgsConstructor
 @ToString
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class Car {
+@Document(indexName ="practical-java")
 
+public class Car {
+    @Id
+    private String id;
     private String brand;
     private String color;
     private String type;
 
     private int price;
     private boolean available;
-    @JsonFormat(pattern="dd-MM-yyyy", timezone = "America/New_York")
+
+    @Field(type= FieldType.Date, format= DateFormat.date)
+    @JsonFormat(pattern="yyyy-MM-dd", timezone = "America/New_York")
     private LocalDate firstRelease;
     @JsonInclude(value= JsonInclude.Include.NON_EMPTY)
     private String secretFeature;
@@ -35,6 +45,5 @@ public class Car {
     @JsonUnwrapped
     private Engine engine;
     private List<Tire> tires;
-
 
 }
