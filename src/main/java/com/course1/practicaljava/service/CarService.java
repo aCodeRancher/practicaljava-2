a92f3c5622c80324b6e2698c3a635184021cb69c
helen <hhma@alumni.cpp.edu>
2022-07-03 22:27:34 -0400

    add more brands, color and types

--- src/main/java/com/course1/practicaljava/service/CarService.java
+++ src/main/java/com/course1/practicaljava/service/CarService.java
@@ -5,9 +5,9 @@ import com.course1.practicaljava.api.server.entity.Car;
 import java.util.List;
 
 public interface CarService {
-    List<String> BRANDS = List.of("Toyota", "Honda", "Ford");
-    List<String> COLORS = List.of("Red", "Black", "white");
-    List<String> TYPES = List.of("Sedan", "SUV", "MPV");
+    List<String> BRANDS = List.of("Toyota", "Honda", "Ford", "BMW", "Mitsibushi");
+    List<String> COLORS = List.of("Red", "Black", "white", "blue", "silver");
+    List<String> TYPES = List.of("Sedan", "SUV", "MPV", "Hatchback", "Convertible");
     List<String> ADDITIONAL_FEATURES = List.of("GPS", "Alarm", "Sunroof", "Media player", "Leather seats");
     List<String> FUELS = List.of("Petrol", "Electric", "Hybrid");
     List<String> TIRE_MANUFACTURERS = List.of("Goodyear", "Bridgestone", "Dunlop");
