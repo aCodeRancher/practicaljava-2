024ad348d61277f1333cafeb757887fa35f7dd45
helen <hhma@alumni.cpp.edu>
2022-07-04 17:56:39 -0400

    add findByBrandAndColor

--- src/main/java/com/course1/practicaljava/repository/CarElasticRepository.java
+++ src/main/java/com/course1/practicaljava/repository/CarElasticRepository.java
@@ -4,8 +4,13 @@ import com.course1.practicaljava.api.server.entity.Car;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarElasticRepository extends ElasticsearchRepository<Car, String> {
+
+ List<Car> findByBrandAndColor(String brand, String color );
+
 
 
 }
