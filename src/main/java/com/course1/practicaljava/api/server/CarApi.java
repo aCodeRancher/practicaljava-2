e217b21ddd7f21a1dc1aff48d8346901a507208c
helen <hhma@alumni.cpp.edu>
2022-07-04 17:55:38 -0400

    add count car and find cars methods

--- src/main/java/com/course1/practicaljava/api/server/CarApi.java
+++ src/main/java/com/course1/practicaljava/api/server/CarApi.java
@@ -1,10 +1,14 @@
 package com.course1.practicaljava.api.server;
 
 import com.course1.practicaljava.api.server.entity.Car;
+import com.course1.practicaljava.repository.CarElasticRepository;
 import com.course1.practicaljava.service.CarService;
+import org.elasticsearch.search.aggregations.metrics.CardinalityAggregationBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +24,9 @@ public class CarApi {
     private static final Logger LOG = LoggerFactory.getLogger(CarApi.class);
     @Autowired
     private CarService carService;
+
+    @Autowired
+    private CarElasticRepository carElasticRepository;
 
     @GetMapping(value="/random" , produces = MediaType.APPLICATION_JSON_VALUE)
      public Car random(){
@@ -43,4 +50,42 @@ public class CarApi {
           return result;
      }
 
+     @GetMapping(value="/count")
+     public String countCar(){
+        return "There are : "+ carElasticRepository.count() + " cars";
+     }
+
+     @PostMapping (value = "", consumes=MediaType.APPLICATION_JSON_VALUE)
+     public String saveCar(@RequestBody Car car){
+         var id = carElasticRepository.save(car).getId();
+         return "Saved with ID: " + id;
+     }
+
+     @GetMapping(value = "/{id}")
+     public Car getCar(@PathVariable("id") String carId){
+        return carElasticRepository.findById(carId).orElse(null);
+
+     }
+
+     @PutMapping(value="/{id}")
+     public String updateCar(@PathVariable("id") String carId, @RequestBody Car updatedCar){
+        updatedCar.setId(carId);
+        var newCar = carElasticRepository.save(updatedCar);
+        return "Updated car with ID : " + newCar.getId();
+     }
+
+    @GetMapping(value = "/find-json", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Car> findCarsByBrandAndColor(@RequestBody Car car) {
+          return carElasticRepository.findByBrandAndColor(car.getBrand(), car.getColor());
+    }
+
+    @GetMapping(value="/cars/{brand}/{color}")
+    public List<Car> findCarsByPath(@PathVariable String brand, @PathVariable String color){
+        return carElasticRepository.findByBrandAndColor(brand, color);
+    }
+
+    @GetMapping(value = "/cars")
+    public List<Car> findCarsByParam(@RequestParam String brand, @RequestParam String color){
+        return carElasticRepository.findByBrandAndColor(brand, color);
+    }
 }
