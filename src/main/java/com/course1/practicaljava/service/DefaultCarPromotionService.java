d1bc4c2cf067c4634dbc1b5c56dfd0df275bc11b
helen <hhma@alumni.cpp.edu>
2022-07-10 12:34:02 -0400

    Add DefaultCarPromotionService

---
+++ src/main/java/com/course1/practicaljava/service/DefaultCarPromotionService.java
@@ -0,0 +1,15 @@
+package com.course1.practicaljava.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultCarPromotionService implements CarPromotionService {
+
+
+    @Override
+    public boolean isValidPromotionType(String promotionType){
+        return PROMOTION_TYPES.contains(promotionType.toLowerCase());
+    }
+
+
+}
