e182ee7a8cbfc4229291bc81a0530af2135bdecd
helen <hhma@alumni.cpp.edu>
2022-07-11 22:04:01 -0400

    add tests for DefaultRestApiTest

--- src/test/java/com/course1/practicaljava/api/server/DefaultRestApiTest.java
+++ src/test/java/com/course1/practicaljava/api/server/DefaultRestApiTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import java.time.LocalTime;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DefaultRestApiTest {
@@ -17,18 +19,49 @@ class DefaultRestApiTest {
         webTestClient.get().uri("/api/welcome")
                 .exchange().expectStatus().is2xxSuccessful().expectBody(String.class)
                 .value(IsEqualIgnoringCase.equalToIgnoringCase("welcome to spring boot"));
-
     }
 
     @Test
     void time() {
+        webTestClient.get().uri("/api/time").exchange()
+                .expectBody(String.class).value(v-> isCorrectTime(v));
+    }
+
+    private Object isCorrectTime(String v){
+        var responseLocalTime = LocalTime.parse(v);
+        var now = LocalTime.now();
+        var nowMinus30Seconds = now.minusSeconds(30);
+        assertTrue(responseLocalTime.isAfter(nowMinus30Seconds)
+                && responseLocalTime.isBefore(now));
+      return responseLocalTime;
     }
 
     @Test
     void headerByAnnotation() {
-    }
+        var headerOne = "Spring Boot Test";
+        var headerTwo = "Spring Boot Test on Practical Java";
+        webTestClient.get().uri("/api/header-one")
+                .header("User-agent", headerOne)
+                .header("Practical-Java", headerTwo)
+                .exchange().expectBody(String.class)
+                .value( v -> {
+                             assertTrue(v.contains(headerOne));
+                              assertTrue(v.contains(headerTwo));
+                            }
+                  );
+     }
 
     @Test
     void headerByRequest() {
+         var header1 = "Spring Boot Test";
+         var header2 = "Spring Boot Test on Practical Java";
+        webTestClient.get().uri("/api/header-two")
+                .header("User-agent", header1)
+                .header("Practical-Java", header2)
+                .exchange().expectBody(String.class)
+                .value( v-> {
+                         assertTrue(v.contains(header1));
+                         assertTrue(v.contains(header2));
+                });
     }
 }
