2a138b88f2a30b42267a6cc3f0670fe82d36f235
helen <hhma@alumni.cpp.edu>
2022-07-14 22:37:26 -0400

    add all tests for each method

--- src/test/java/com/course1/practicaljava/api/server/CarApiTest.java
+++ src/test/java/com/course1/practicaljava/api/server/CarApiTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.Duration;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,6 +39,9 @@ class CarApiTest {
 
     @Test
     void randomCars() {
+        webTestClient.get().uri("/api/car/v1/random-cars")
+                .exchange().expectBodyList(Car.class)
+                .value( cars -> assertTrue(cars.size()>0));
     }
 
     @Test
@@ -63,14 +68,69 @@ class CarApiTest {
 
     @Test
     void getCar() {
+        String dateofRelease  = "2010-01-01";
+        AtomicReference<Car> firstCar = new AtomicReference<>();
+        //get the first car from the output
+        webTestClient.get().uri(uriBuilder-> uriBuilder.path("/api/car/v1/cars/date")
+                        .queryParam("first_release_date", dateofRelease).build())
+                        .exchange().expectBodyList(Car.class).value(
+                        cars -> {
+                            firstCar.set(cars.get(0));
+                        }
+                );
+        webTestClient.get().uri(uriBuilder -> uriBuilder.path("/api/car/v1/{id}")
+                        .build(firstCar.get().getId()))
+                .exchange().expectBody(Car.class).value(
+                        car -> assertTrue( car.getId().equals(firstCar.get().getId()))
+                );
     }
 
     @Test
     void updateCar() {
+        String dateofRelease  = "2010-01-01";
+        AtomicReference<Car> firstCar = new AtomicReference<>();
+       //get the first car from the output
+        webTestClient.get().uri(uriBuilder-> uriBuilder.path("/api/car/v1/cars/date")
+                        .queryParam("first_release_date", dateofRelease).build())
+                .exchange().expectBodyList(Car.class).value(
+                        cars -> {
+                            firstCar.set(cars.get(0));
+                        }
+                );
+        //update the first car's color to cyan
+         Car updatedCar = firstCar.get();
+         final String newColor = "cyan";
+         updatedCar.setColor(newColor);
+        webTestClient.put().uri(uriBuilder -> uriBuilder.path("/api/car/v1/{id}")
+                        .build(firstCar.get().getId()))
+                        .bodyValue(updatedCar).exchange().expectStatus().is2xxSuccessful();
+        //get the updated car color
+        webTestClient.get().uri(uriBuilder -> uriBuilder.path("/api/car/v1/{id}")
+                        .build(firstCar.get().getId()))
+                         .exchange().expectBody(Car.class).value(
+                                  car -> assertTrue( car.getColor().equals(newColor))
+                );
     }
 
     @Test
     void findCarsByBrandAndColor() {
+        Car car = new Car();
+        final String brand = "Toyota";
+        final String color = "Red";
+        car.setBrand(brand);
+        car.setColor(color);
+        webTestClient.method(HttpMethod.GET).uri("/api/car/v1/find-json")
+                .bodyValue(car).accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBodyList(Car.class).value(cars ->
+                  {
+                    cars.forEach(c -> {
+                        assertTrue(c.getBrand().equals(brand));
+                        assertTrue(c.getColor().equals(color));
+                    });
+
+                });
     }
 
     @Test
