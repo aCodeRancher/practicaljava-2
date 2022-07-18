e1c93d8ce5d3e69493e2b89c1fc3422295b276b3
helen <hhma@alumni.cpp.edu>
2022-07-17 22:00:25 -0400

    add CarPromotion

---
+++ src/main/java/com/course1/practicaljava/api/server/entity/CarPromotion.java
@@ -0,0 +1,23 @@
+package com.course1.practicaljava.api.server.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName="practical-java-2")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarPromotion {
+    private String type;
+
+    private String description;
+
+    @Id
+    private String id;
+
+}
